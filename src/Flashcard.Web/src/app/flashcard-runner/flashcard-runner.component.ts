59f54774bd044815257e3193726926cfc7c6967e
Fredrik Lundin <fredrik.lundin@capgemini.com>
2016-11-02 23:02:24 +0100

    initial work on views

--- src/Flashcard.Web/src/app/flashcard-runner/flashcard-runner.component.ts
+++ src/Flashcard.Web/src/app/flashcard-runner/flashcard-runner.component.ts
@@ -2,20 +2,38 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { FlashcardRunnerService } from './flashcard-runner.service';
+import { Flashcard } from './flashcard';
 
 @Component({
   selector: 'app-flashcard-runner',
   templateUrl: './flashcard-runner.component.html',
-  styleUrls: ['./flashcard-runner.component.css']
+  styleUrls: ['./flashcard-runner.component.scss']
 })
 export class FlashcardRunnerComponent implements OnInit, OnDestroy {
   private _sub: Subscription;
-  private Data;
+  private flashcards: Flashcard[];
+  private flashcard: Flashcard;
+  private flashcardIndex: number;
+  private showAnswer: boolean;
 
   constructor(private _service: FlashcardRunnerService) { }
 
   ngOnInit() {
-    this._sub = this._service.getCards().subscribe(res => this.Data = res);
+    this.showAnswer = false;
+    this.flashcardIndex = 0;
+
+    this._sub = this._service.getCards().subscribe(res => {
+      this.flashcards = res;
+      this.flashcard = this.flashcards[this.flashcardIndex];
+    });
+  }
+
+  nextCard() {
+    let maxLength = this.flashcards.length;
+    if ((this.flashcardIndex + 1) < maxLength) {
+      this.flashcard = this.flashcards[++this.flashcardIndex];
+      this.showAnswer = false;
+    }
   }
 
   ngOnDestroy() {
