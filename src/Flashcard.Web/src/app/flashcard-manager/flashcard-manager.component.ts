65f26a9edeb3de3a6c48abf0b23795198c3c0fbe
Fredrik Lundin <fredrik.lundin@capgemini.com>
2016-11-13 19:59:09 +0100

    Adding flashcard working (not done)

--- src/Flashcard.Web/src/app/flashcard-manager/flashcard-manager.component.ts
+++ src/Flashcard.Web/src/app/flashcard-manager/flashcard-manager.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Flashcard } from './../flashcard-runner/flashcard';
+import { FlashcardRunnerService } from './../flashcard-runner/flashcard-runner.service';
+
 
 @Component({
   selector: 'app-flashcard-manager',
@@ -6,10 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flashcard-manager.component.scss']
 })
 export class FlashcardManagerComponent implements OnInit {
+  private showNewCardForm: boolean;
+  private addingCard: boolean;
+  private addedCardErrorMessage: string;
+  private addedCardSuccessMessage: string;
 
-  constructor() { }
+  constructor(private service: FlashcardRunnerService) { }
 
   ngOnInit() {
+    this.resetFormState();
   }
 
+  showForm() {
+    this.resetFormState();
+    this.showNewCardForm = true;
+  }
+
+  resetFormState() {
+    this.showNewCardForm = false;
+    this.addingCard = false;
+    this.addedCardErrorMessage = '';
+    this.addedCardSuccessMessage = '';
+  }
+
+  onSubmit({value, valid}: { value: Flashcard, valid: boolean }) {
+    this.addingCard = true;
+
+    this.service.addNewCard(value)
+      .subscribe(res => {
+        this.addedCardSuccessMessage = 'New card added successfully';
+        this.addingCard = false;
+        setTimeout(() => this.showNewCardForm = false, 2000);
+      },
+      error => {
+        this.addedCardErrorMessage = 'An error prevented the flashcard to be added - sorry about that!';
+        this.addingCard = false;
+      });
+  }
 }
--- src/Flashcard.Web/src/app/flashcard-runner/flashcard-runner.service.ts
+++ src/Flashcard.Web/src/app/flashcard-runner/flashcard-runner.service.ts
@@ -1,17 +1,41 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Observable } from 'rxjs';
+
 
 import { Flashcard } from './flashcard';
 
 @Injectable()
 export class FlashcardRunnerService {
+    private apiUrl = '/api/flashcard';
 
     constructor(private _http: Http) { }
 
-    getCards() : Observable<Flashcard[]> {
-        return this._http.get('/api/flashcard')
-            .map(res => res.json());
+    getCards(): Observable<Flashcard[]> {
+        return this._http.get(this.apiUrl)
+            .map(res => res.json())
+            .catch(this.handleError);
     }
+
+    addNewCard(flashcard: Flashcard) {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+
+        return this._http.post(this.apiUrl, flashcard, options)
+            .catch(this.handleError);
+    }
+
+    handleError (error: Response | any) {
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+
+    return Observable.throw(errMsg);
+  }
 
 }
